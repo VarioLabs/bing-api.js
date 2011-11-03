487986069a238f03b0fba1549af353de543e2324
sujal shah <codesujal@gmail.com>
2011-11-03 16:20:28 -0400

    eliminate test code that shouldn't get hit

--- test/search-client-test.js
+++ test/search-client-test.js
@@ -4,24 +4,7 @@ client.search("variolabs", function(error, response, data) {
   
   if (!error && response.statusCode == 200)
   {
-    if (data === null || data.SearchResponse === null || (data.SearchResponse.Errors && data.SearchResponse.Errors.length > 0))
-    {
-      if (error === null)
-        error = ""
-        
-      for (var i = data.SearchResponse.Errors.length - 1; i >= 0; i--){
-        error += data.SearchResponse.Errors[i].Message;
-        if (data.SearchResponse.Errors[i].Parameter)
-        {
-          error += " ("+data.SearchResponse.Errors[i].Parameter+")";
-        }
-      };
-            
-      console.log("ERROR executing search: "+error);
-    } else {
-      console.log("SUCCESS: Found " + data.SearchResponse.Web.Results.length + " results");
-    }
-    
+    console.log("SUCCESS: Found " + data.SearchResponse.Web.Results.length + " results");
   } else {
     console.log("ERROR! " + error + "/" + response.statusCode);
   }
